ec715efc6db6e36d087eaddc9c53e26df26d86d1
Tore Norderud <tore.norderud@gmail.com>
2014-09-05 21:27:37 +0200

    Legger til sharebar.php og bruker på single.php.

---
+++ wp-content/themes/radikalportal/sharebar.php
@@ -0,0 +1,15 @@
+<div style="background-color:#f9f9f9;border-style:solid;border-color:#eeeeee;border-width:2px 0px 0px 0px;padding:4px 0px 0px 4px;">
+  <div style="float:left;padding:0px 0px;">
+    <a class="btn btn-small" href="javascript:print();"><i class="icon-print"></i>Skriv ut</a>
+  </div>
+  <div style="float:left;padding:3px 0px 0px 14px;">
+    <div class="fb-like" data-href="<?= get_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+  </div>
+  <div style="float:left;padding:3px 0px 0px 14px;">
+    <?php dd_twitter_generate('Compact','radikalportal') ?>
+  </div>
+  <div style="float:left;padding:1px 0px;margin-left:-14px;">
+    <?php dd_google1_generate('Compact (24px)') ?>
+  </div>
+  <div class="clearfix"></div>
+</div>
--- wp-content/themes/radikalportal/single.php
+++ wp-content/themes/radikalportal/single.php
@@ -39,30 +39,16 @@
 
 <div class="span18">
   <?php get_template_part('author', 'inline'); ?>
+  <?php get_template_part('sharebar'); ?>
+  <br>
+  <b>Publisert: <?php the_date("d.m.y"); ?></b>
 </div>
 
-      <div class="span15">
-        <div style="background-color:#f9f9f9;border-style:solid;border-color:#eeeeee;border-width:2px 0px 0px 0px;padding:4px 0px 0px 4px;">
-	  <div style="float:left;padding:0px 0px;">
-            <a class="btn btn-small" href="javascript:print();"><i class="icon-print"></i>Skriv ut</a>
-	  </div>
-	  <div style="float:left;padding:3px 0px 0px 14px;">
-        <div class="fb-like" data-href="<?= get_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
-	  </div>
-	  <div style="float:left;padding:3px 0px 0px 14px;">
-            <?php dd_twitter_generate('Compact','radikalportal') ?>
-	  </div>
-	  <div style="float:left;padding:1px 0px;margin-left:-14px;">
-            <?php dd_google1_generate('Compact (24px)') ?>
-	  </div>
-	  <div class="clearfix"></div>
-        </div>
-      </div>
-      <br>
-      <div class="span15">
-        <div class="entry">
-          <br>
-          <?php the_content(); ?>
+<div class="span15">
+  <div class="entry">
+    <br>
+<?php the_content(); ?>
+
 <?php
 $faktabokser = get_post_custom_values('faktaboks');
 if (isset($faktabokser)) {
@@ -71,62 +57,47 @@ if (isset($faktabokser)) {
 	}
 }
 ?>
-          <?php if (!strcmp(get_post_meta($post->ID, 'ingendiskusjon', true), "1") == 0) { ?>
 
-        <div style="background-color:#f9f9f9;border-style:solid;border-color:#eeeeee;border-width:2px 0px 0px 0px;padding:4px 0px 0px 4px;">
-	  <div style="float:left;padding:0px 0px;">
-            <a class="btn btn-small" href="javascript:print();"><i class="icon-print"></i>Skriv ut</a>
-	  </div>
-	  <div style="float:left;padding:3px 0px 0px 14px;">
-        <div class="fb-like" data-href="<?= get_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
-	  </div>
-	  <div style="float:left;padding:3px 0px 0px 14px;">
-            <?php dd_twitter_generate('Compact','radikalportal') ?>
-	  </div>
-	  <div style="float:left;padding:1px 0px;margin-left:-14px;">
-            <?php dd_google1_generate('Compact (24px)') ?>
-	  </div>
-	  <div class="clearfix"></div>
-        </div>
-        </div>
-      </div>
-      <div class="span8">
-        <h3 class="page-header">Tips oss</h3>
-          <div class="textwidget"><p>Har du et tips om en sak du mener vi burde skrive om, eller en lenke vi burde dele på siden så send oss et tips <a href="http://radikalportal.no/tips-oss/">HER</a></p></div>
-      </div>
-	<div class="span7">
-		<h3 class="page-header">Følg oss på Facebook</h3>
-		<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fradikalportal&amp;width=292&amp;height=62&amp;show_faces=false&amp;colorscheme=light&amp;stream=false&amp;border_color=%23ffffff&amp;header=false&amp;appId=446811972038512" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:62px;" allowTransparency="true"></iframe>
-	</div>
-      <div class="span15">
-        <h2 class="page-header">Diskusjon</h2>
-        <div class="well debattregler">
-          <p>
-            <strong>DEBATTREGLER:</strong>
-            <br>
-            - Respekter dine meddebattanter og utøv normal folkeskikk<br>
-            - Vær saklig og hold deg til tema<br>
-            - Ta ballen – ikke spilleren!
-          </p>
-          <p>Vi fjerner innlegg som er diskriminerende, hetsende og usaklige, spam og identiske kommentarer.</p>
-        </div>
-        <div id="disqus_thread">
-        </div>
-        <script type="text/javascript">
-	        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-	        var disqus_shortname = 'radikalportal'; // required: replace example with your forum shortname
+  </div> <!-- /.entry -->
+</div> <!-- /.span15 -->
 
-	        /* * * DON'T EDIT BELOW THIS LINE * * */
-	        (function() {
-	            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-	            dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-	            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-	        })();
-        </script>
-        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-      </div>
-<?php } ?>
+<div class="span18">
+  <?php get_template_part('sharebar'); ?>
+  <h3 class="page-header">Tips oss</h3>
+  Har du et tips om en sak du mener vi burde skrive om, eller en lenke vi burde dele på siden så send oss et <a href="http://radikalportal.no/tips-oss/">tips</a>.
+</div>
+
+<?php if (!strcmp(get_post_meta($post->ID, 'ingendiskusjon', true), "1") == 0) : ?>
+<div class="span18">
+  <h3 class="page-header">Diskusjon</h3>
+  <div class="well debattregler">
+    <p>
+      <strong>DEBATTREGLER:</strong>
+      <br>
+      - Respekter dine meddebattanter og utøv normal folkeskikk<br>
+      - Vær saklig og hold deg til tema<br>
+      - Ta ballen – ikke spilleren!
+    </p>
+    <p>Vi fjerner innlegg som er diskriminerende, hetsende og usaklige, spam og identiske kommentarer.</p>
+  </div>
+  <div id="disqus_thread">
+  </div>
+  <script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'radikalportal'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+  </script>
+  <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+  <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+</div>
+<?php endif; ?>
+
     </div> <!--/.row -->
   </div> <!--/.post -->
 <?php endwhile; else: ?>
