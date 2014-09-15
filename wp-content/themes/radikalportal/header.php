4dbb290fc35e2bae57ad658d222f0c0ba1aeafd9
Tore Norderud <tore.norderud@gmail.com>
2014-09-16 01:14:17 +0200

    Flytt style ut av header.php.

--- wp-content/themes/radikalportal/header.php
+++ wp-content/themes/radikalportal/header.php
@@ -18,83 +18,8 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/journal/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css">
     <link rel="image_src" href="http://radikalportal.no/wp-content/uploads/userphoto/51.jpg" />
-    <style type="text/css">
- 
-.front-page-img img {width: 100%; }
-.post p { color: #000; }
-.single p { }
-.sidebar { font-size: 12px; }
-  .nav-tabs > .active > a, .nav-tabs > .active > a:hover {
-    background-color: #fcfcfc;
-    color: #000000;
-  }
-
-@media print {
-  .navbar, .disqus, .print-hidden {
-    display: none;
-  }
-  a { color: #000; }
-}
-
-.infobox {
-  border: 1px solid #ccc;
-  background-color: #f8f8f8;
-  color: #000;
-  padding: 0 0px 10px 0px;
-  font-size: 84%;
-  font-family: verdana, sans-serif;
-  line-height: 150%;
-  clear:right;
-  float: right;
-  width: 300px;
-  margin: 0 0 9px 20px;
-}
-.wideinfo{
-  border: 1px solid #ccc;
-  background-color: #f8f8f8;
-  color: #000;
-  padding: 0 0px 10px 0px;
-  font-size: 84%;
-  font-family: verdana, sans-serif;
-  line-height: 150%;
-  clear:right;
-  float: left;
-  width: 100%;
-  margin: 0 0 9px 20px;
-}
-.infobox hr { display: none; }
-.infobox p { margin: 10px 0 0 0;}
-.infobox img { margin: 10px 0 0 10px; padding: 10px; 0 0 20px; width:80px; height:80px; }
-.infobox h3,
-.infobox h4 {
-  display: block;
-  padding: 3px 10px 5px 10px;
-  margin: 0px -10px 10px -10px;
-  font-size: 100%;
-  font-weight: bold;
-  line-height: 100%;
-  background-color: #ddd;
-  color: #000;
-}
-.infobox h4 {
-  margin: 10px -10px 10px -10px;
-  background-color: #eee;
-  color: #000;
-}
-.infobox br { display: none }
-
-.navbar .nav > li > a {
-	font-size:16px;
-}
-
-.sidebar li
-{
-	font-size: 8pt;
-	line-height: 13pt;
-}
-
-    </style>
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
