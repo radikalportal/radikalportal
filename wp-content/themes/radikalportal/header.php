88cad9a73aa804eff18ab6b18044ed2fe48f6c9f
Tore Norderud <tore.norderud@gmail.com>
2014-09-18 10:48:56 +0200

    Liten justering av style.css.

--- wp-content/themes/radikalportal/header.php
+++ wp-content/themes/radikalportal/header.php
@@ -18,7 +18,7 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/journal/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css?1">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css?2">
     <link rel="image_src" href="http://radikalportal.no/wp-content/uploads/userphoto/51.jpg" />
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
