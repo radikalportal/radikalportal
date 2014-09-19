9214d5e99e1fbd0c34fc70e702885e8d4d8c901d
Tore Norderud <tore.norderud@gmail.com>
2014-09-19 21:43:06 +0200

    Fikser support for IE8.

--- wp-content/themes/radikalportal/header.php
+++ wp-content/themes/radikalportal/header.php
@@ -20,10 +20,12 @@
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/journal/bootstrap.min.css">
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css?3">
     <link rel="image_src" href="http://radikalportal.no/wp-content/uploads/userphoto/51.jpg" />
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+
     <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
     <?php wp_head(); ?>
     <link rel="alternate" type="application/rss+xml" title="Artikler (RSS)" href="<?php bloginfo('rss2_url'); ?>" />
     <script type="text/javascript">
