5c2d26fee957bc10c5e37f31b867dc6da55ed6a9
Tore Norderud <tore.norderud@gmail.com>
2015-02-18 18:52:04 +0100

    Sporing for Blogglisten.

--- wp-content/themes/radikalportal/footer.php
+++ wp-content/themes/radikalportal/footer.php
@@ -2,6 +2,8 @@
 
 <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
 
+<img height="0" border="0" width="0" alt="hits" src="http://hits.blogsoft.org/?eid=721" />
+
 <script type="text/javascript">
 jQuery(document).ready(function($) {
     $('.carousel').carousel({
