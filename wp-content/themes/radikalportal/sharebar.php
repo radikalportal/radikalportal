04fec4a62e504a2efc4019179ddfc36190ae498c
Tore Norderud <tore.norderud@gmail.com>
2015-03-08 20:58:14 +0100

    Fjern digg-digg og omskrevet Twitter-knapp i sharebar.php.

--- wp-content/themes/radikalportal/sharebar.php
+++ wp-content/themes/radikalportal/sharebar.php
@@ -3,7 +3,8 @@
     <div class="fb-like" data-href="<?= get_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
   </div>
   <div style="float:left;padding:3px 0px 0px 14px;">
-    <?php dd_twitter_generate('Compact','radikalportal') ?>
+    <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" data-via="radikalportal" data-lang="no" data-dnt="true">Tweet</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
   </div>
   <a class="hidden-xs btn btn-default btn-xs" style="float: right;" href="javascript:if(window.print)window.print()"><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Skriv ut</a>
   <div class="clearfix"></div>
