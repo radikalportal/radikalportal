5de01fdf826f554d35ff5e1d7f7529b90436ae03
Tore Norderud <tore.norderud@gmail.com>
2015-05-31 23:56:05 +0200

    Vis featured image på pages.

--- wp-content/themes/radikalportal/page.php
+++ wp-content/themes/radikalportal/page.php
@@ -8,6 +8,12 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+		<?php if ( has_post_thumbnail() ) : ?>
+		<a class="front-page-img" href="<?php the_permalink() ?>">
+		<?php the_post_thumbnail('large'); ?>
+		</a>
+		<?php endif; ?>
+
 		<div class="page-header">
 			<h1><?php the_title(); ?></h1>
 		</div>
