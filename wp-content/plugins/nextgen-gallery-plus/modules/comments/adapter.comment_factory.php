f671bb30fc7f91fe3070038ecc08fd54ed95699a
Tore Norderud <tore.norderud@gmail.com>
2015-06-28 22:58:52 +0200

    Revert "Nextgen-gallery-plus plugin lagt til"
    This reverts commit 35d33397baf315153f9c2fb65fe3ae92c978fd56.

--- wp-content/plugins/nextgen-gallery-plus/modules/comments/adapter.comment_factory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class A_Comment_Factory extends Mixin
-{
-    function comment_container($properties=array(), $mapper=FALSE, $context=FALSE)
-    {
-        return new C_Comment_Container($properties, $mapper, $context);
-    }
-}
--- wp-content/plugins/nextgen-gallery-plus/modules/nextgen_pro_thumbnail_grid/templates/nextgen_pro_thumbnail_grid_dyncss.php
+++
@@ -1,9 +0,0 @@
-<?php if ($border_size): ?>
-#<?php echo $id ?> img { border: solid <?php echo $border_size ?>px <?php echo $border_color ?>; }
-<?php else: ?>
-#<?php echo $id ?> img { border: none; }
-<?php endif ?>
-
-#<?php echo $id ?> .image-wrapper {
-	margin: <?php echo $spacing ?>px;
-}
