f50b625611df7b378f1ffe8b16fef7b3a1a31cf3
Tore Norderud <tore.norderud@gmail.com>
2015-07-12 23:33:17 +0200

    Indenter page-osloportalen.php.

--- wp-content/themes/radikalportal/page-osloportalen.php
+++ wp-content/themes/radikalportal/page-osloportalen.php
@@ -7,50 +7,50 @@ query_posts('cat=2111&paged=' . $paged);
 
 <div class="row">
 
-  <div class="col-md-3 hidden-xs hidden-sm">
-    <?php get_sidebar(left); ?>
-  </div>
+    <div class="col-md-3 hidden-xs hidden-sm">
+        <?php get_sidebar(left); ?>
+    </div>
 
-  <div id="main-content" class="main-content col-md-9">
-	<div class="page-header">
-		<h1><?php the_title(); ?></h1>
-	</div>
+    <div id="main-content" class="main-content col-md-9">
+        <div class="page-header">
+            <h1><?php the_title(); ?></h1>
+        </div>
 
-    <div id="primary" class="content-area">
-      <div id="content" class="site-content" role="main">
+        <div id="primary" class="content-area">
+            <div id="content" class="site-content" role="main">
 
-    <?php
-      if ( have_posts() ) :
-        // Start the Loop.
-	    $first = true;
-        while ( have_posts() ) : the_post();
+                <?php
+                if ( have_posts() ) :
+                // Start the Loop.
+                $first = true;
+                while ( have_posts() ) : the_post();
 
-          /*
-           * Include the post format-specific template for the content. If you want to
-           * use this in a child theme, then include a file called called content-___.php
-           * (where ___ is the post format) and that will be used instead.
-           */
-          if ($first) {
-	          get_template_part( 'content-oslo-featured', get_post_format() );
-          } else {
-	          get_template_part( 'content-oslo', get_post_format() );
-          }
-          $first = false;
+                /*
+                 * Include the post format-specific template for the content. If you want to
+                 * use this in a child theme, then include a file called called content-___.php
+                 * (where ___ is the post format) and that will be used instead.
+                 */
+                if ($first) {
+                    get_template_part( 'content-oslo-featured', get_post_format() );
+                } else {
+                    get_template_part( 'content-oslo', get_post_format() );
+                }
+                $first = false;
 
-        endwhile;
+                endwhile;
 
-        get_template_part( 'pagination' );
+                get_template_part( 'pagination' );
 
-      else :
-        // If no content, include the "No posts found" template.
-        get_template_part( 'content', 'none' );
+                else :
+                         // If no content, include the "No posts found" template.
+                         get_template_part( 'content', 'none' );
 
-      endif;
-    ?>
+                endif;
+                ?>
 
-      </div><!-- #content -->
-    </div><!-- #primary -->
-  </div><!-- #main-content -->
+            </div><!-- #content -->
+        </div><!-- #primary -->
+    </div><!-- #main-content -->
 </div>
 
 <?php get_footer(); ?>
