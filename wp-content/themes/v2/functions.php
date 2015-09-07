eddfbaeee95dd197eb63665a54560f86e86b0ccf
Tore Norderud <tore.norderud@gmail.com>
2015-09-07 19:06:04 +0200

    Fjern logo custom.

--- wp-content/themes/v2/functions.php
+++ wp-content/themes/v2/functions.php
@@ -8,14 +8,6 @@ function mh_magazine_child_styles() {
 }
 add_action('wp_enqueue_scripts', 'mh_magazine_child_styles');
 
-function mh_logo() {
-    echo '<div style="background-color: #ef4030; margin: 20px 20px; padding: 10px 15px; display: inline-block;">';
-    echo '<h1>';
-    echo '<a style="color: #fff;" href="/">Radikal Portal</a>';
-    echo '</h1>';
-    echo '</div>';
-}
-
 function mh_scripts() {
     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'));
     if (!is_admin()) {
