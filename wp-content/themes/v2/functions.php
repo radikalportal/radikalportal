fab9202251ce588a615b32f9a4c11cf922f05289
Tore Norderud <tore.norderud@gmail.com>
2015-10-12 20:01:08 +0200

    Enda en ny fiks av Facebook-widget (closes #15).

--- wp-content/themes/v2/functions.php
+++ wp-content/themes/v2/functions.php
@@ -22,7 +22,7 @@ function mh_share_buttons_content() {
 
 function mh_magazine_child_styles() {
     wp_enqueue_style('mh-magazine-parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('mh-magazine-child-style', get_stylesheet_directory_uri() . '/style.css', array('mh-magazine-parent-style'), '151008');
+    wp_enqueue_style('mh-magazine-child-style', get_stylesheet_directory_uri() . '/style.css', array('mh-magazine-parent-style'), '151012');
 }
 add_action('wp_enqueue_scripts', 'mh_magazine_child_styles');
 
